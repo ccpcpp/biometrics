c7846726b17b29c6cf9219c1b30b3bd1070f47ba
heyyeh12 <heyyeh12@aol.com>
2015-10-24 23:01:10 -0500

    added batch version...parameters still need tuning

--- iris_segmentation/hough_circle.py
+++ iris_segmentation/hough_circle.py
@@ -1,83 +1,90 @@
 #!/usr/bin/env python
 import sys
+import os.path
 import cv2
 import cv2.cv as cv
 import numpy as np
 
-# callback for trackbar
-def nothing(x):
-    pass
 
-## GLOBAL VARS ##
-dp = 1
-minD = 20
-p1 = 50
-p2 = 30
-minR = 30
-maxR = 75
-loop = 0
+## DEFAULT PARAMETERS ##
+houghParamsA = dict(    dp = 1, # ratio of image res to accumulator res
+                        minDist = 500, #min dist btwn circle centers
+                        param1 = 51, #upper canny thresh
+                        param2 = 8, #lower canny thresh
+                        minRadius = 62, #min circle radius
+                        maxRadius = 195 #max circle radius
+                        )
 
-## TESTING ##
-cv2.namedWindow('hough')
-cv2.createTrackbar('p1', 'hough', 1, 100, nothing)
-cv2.createTrackbar('p2', 'hough', 1, 100, nothing)
-cv2.createTrackbar('minR', 'hough', 0, 20, nothing)
-cv2.createTrackbar('maxR', 'hough', 1, 100, nothing)
-
-
-## TUNING PARAMETERS ##
-hough_params = dict( #dp = 1, #inverse ratio of accumulator : img resolution (1 for same res as img, 2 for 1/2 width & height of img)
-                     # min_dist = 20,
-                     param1 = p1, #upper threshold for
-                     param2 = p2, #accumulator threshold
-                     minRadius = minR, #min circle radius
-                     maxRadius = maxR #max circle radius
-                     )
-
+houghParamsB = dict(    dp = 1, # ratio of image res to accumulator res
+                        minDist = 500, #min dist btwn circle centers
+                        param1 = 69, #upper canny thresh
+                        param2 = 8, #lower canny thresh
+                        minRadius = 62, #min circle radius
+                        maxRadius = 195 #max circle radius
+                        )
 
 if __name__ == '__main__':
 
     try:
-        # get image specified in command line argument
-        img = cv2.imread(sys.argv[1], cv2.IMREAD_GRAYSCALE)
+        imgDir = sys.argv[1]
+        saveDir = sys.argv[2]
+        print "images: " + imgDir + ", output: " + saveDir
     except:
-        # get hard coded image if not specified...should be in same directory as executed code)
-        img = cv2.imread('02463d1926.tiff', cv2.IMREAD_GRAYSCALE)
+        print "no directory specified, using default: images output"
+        imgDir = "images"
+        saveDir = "batch_output"
 
-    # smoothing - TODO: try other filters like Canny Edge etc.
-    bimg = cv2.medianBlur(img, 5)
+    path = os.path.join(os.getcwd(), imgDir)
 
-    # make a duplicate color image
-    cimg = cv2.cvtColor(bimg, cv2.COLOR_GRAY2BGR)
+    for filename in os.listdir(path):
+        if not filename.endswith('.tiff'): continue
+        print "opening \'" + filename + "'"
+        img_path = os.path.join(path, filename)
 
-    # compute hough circles
-    circles = cv2.HoughCircles(img, cv.CV_HOUGH_GRADIENT, dp, minD, **hough_params)
+        # get image
+        img = cv2.imread(img_path, cv2.IMREAD_GRAYSCALE)
 
-    for i in circles[0,:]:
-        # draw circles on color image
-        cv2.circle(cimg, (i[0], i[1]), i[2], (0, 255, 0), 2)
-        
-    ## TODO: tune parameters on fly ##
-    # while (1):
-    #     p1 = cv2.getTrackbarPos('p1', 'hough')
-    #     p2 = cv2.getTrackbarPos('p2', 'hough')
-    #     minR = cv2.getTrackbarPos('minR', 'hough')
-    #     maxR = cv2.getTrackbarPos('maxR', 'hough')
-    #     circles = cv2.HoughCircles(img, cv.CV_HOUGH_GRADIENT, dp, minD, param1=p1, param2=p2, minRadius=minR, maxRadius=maxR) #, **hough_params)
-        
-    #     if !circles:
-    #         pass
-    #     else:
-    #         for i in circles[0,:]:
-    #             # print "(" + str(i[0]) + ", " + str(i[1]) + ") " + str(i[2])
-    #             # draw outer circle
-    #             cv2.circle(cimg, (i[0], i[1]), i[2], (0, 255, 0), 2)
-        
-    ## DISPLAY IMAGES ##
-    # cv2.imshow('original', img)
-    # cv2.imshow('blurred', bimg)
-    cv2.imshow('hough', cimg)
-    
-    cv2.waitKey(0)
-    
-    cv2.destroyAllWindows()
+        # smoothing
+        blurred = cv2.GaussianBlur(img, (5, 5), 0)
+        # cv2.imshow('Gaussian', blurred)
+
+        # threshold
+        # ret, thresh = cv2.threshold(blurred, 0, 255, cv2.THRESH_OTSU)
+        # cv2.imshow('thresh', thresh)
+
+        # duplicate original image in color for displaying circles
+        cimgA = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
+
+        # # filter image
+        # edge = cv2.Canny(blurred, **cannyParams)
+        # cv2.imshow('canny', edge)
+        # edge = cv2.Laplacian(blurred, cv2.CV_64F)
+        # cv2.imshow('laplacian', edge)
+
+        # get small hough circles
+        circles = cv2.HoughCircles(blurred, cv.CV_HOUGH_GRADIENT, **houghParamsA)
+        if circles is not None:
+            for i in circles[0,:]:
+                cv2.circle(cimgA, (i[0], i[1]), i[2], (0, 255, 0), 2)
+        else:
+            print "No circles found!"
+
+        # get big hough circles
+        circles = cv2.HoughCircles(blurred, cv.CV_HOUGH_GRADIENT, **houghParamsB)
+        if circles is not None:
+            for i in circles[0,:]:
+                cv2.circle(cimgA, (i[0], i[1]), i[2], (255, 0, 0), 2)
+        else:
+            print "No circles found!"
+
+        # display images
+        # cv2.imshow('hough', cimgA)
+        # cv2.waitKey(0)
+
+        # save image
+        head, tail = os.path.split(img_path)
+        savePath = os.path.join(saveDir, tail)
+        print "saving image to '"+ savePath+'\''
+        cv2.imwrite(savePath, cimgA)
+
+        cv2.destroyAllWindows()
